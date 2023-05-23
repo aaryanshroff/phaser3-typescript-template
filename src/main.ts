ad48bd4d5265e873f5965dbaba34c2f42230cd90
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-05-22 23:06:10 -0400

    helpful comments

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import Phaser from "phaser"
-
-// import scenes here
 import Game from './scenes/Game'
 
 const config: Phaser.Types.Core.GameConfig = {
   scale: {
-    mode: Phaser.Scale.ScaleModes.RESIZE,
-    parent: 'app',
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    mode: Phaser.Scale.ScaleModes.RESIZE, // The game canvas will be resized to match that of the parent container
+    parent: 'app', // The ID, or DOM node ID, of the DOM element into which the canvas created by the renderer will be injected
+    autoCenter: Phaser.Scale.CENTER_BOTH // The canvas will be horizontally and vertically centered within its parent
   },
-  scene: [Game]
+  scene: [Game] // The scene to start with
 }
 
 export default new Phaser.Game(config)
