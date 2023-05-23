4028d4a0ba19a7067b5c9930781dbe07a71c2f84
Aaryan Shroff <aaryan.shroff@uwaterloo.ca>
2023-05-22 23:03:54 -0400

    add game scene

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import Phaser from "phaser"
 
+// import scenes here
+import Game from './scenes/Game'
+
 const config: Phaser.Types.Core.GameConfig = {
   scale: {
     mode: Phaser.Scale.ScaleModes.RESIZE,
     parent: 'app',
     autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+  },
+  scene: [Game]
 }
 
 export default new Phaser.Game(config)
---
+++ src/scenes/Game.ts
@@ -0,0 +1,12 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene {
+  constructor() {
+    // scene key is 'game'
+    super('game')
+  }
+
+  preload() { }
+
+  create() { }
+}
